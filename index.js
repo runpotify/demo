5611e0ba7fc4d623f1e889f80d73c12d7255f468
Han Park <hanpark@Hans-MacBook-Pro.local>
2022-02-05 11:25:45 +0900

    UI Quick Fix

---
+++ index.js
@@ -0,0 +1,77 @@
+$("#imgBtn-0").click(function(){
+    $(".player-btn").css({
+        "color" : "#6c757d",
+        "border-color" : "#6c757d"
+    });
+    
+   $("#btn-0-s, #btn-0-y").css({
+       "color" : "yellow",
+       "border-color" : "yellow",
+       "transition-timing-funtion" : "ease-in",
+   });
+});
+
+$("#imgBtn-1").click(function(){
+    $(".player-btn").css({
+        "color" : "#6c757d",
+        "border-color" : "#6c757d"
+    });
+    $("#btn-1-s, #btn-1-y").css({
+        "color" : "yellow",
+        "border-color" : "yellow",
+        "transition-timing-funtion" : "ease-in",
+    });
+ });
+
+ $("#imgBtn-2").click(function(){
+    $(".player-btn").css({
+        "color" : "#6c757d",
+        "border-color" : "#6c757d"
+    });
+    $("#btn-2-s, #btn-2-y").css({
+        "color" : "yellow",
+        "border-color" : "yellow",
+        "transition-timing-funtion" : "ease-in",
+    });
+ });
+
+ $("#imgBtn-3").click(function(){
+    $(".player-btn").css({
+        "color" : "#6c757d",
+        "border-color" : "#6c757d"
+    });
+    $("#btn-3-s, #btn-3-y").css({
+        "color" : "yellow",
+        "border-color" : "yellow",
+        "transition-timing-funtion" : "ease-in",
+    });
+ });
+
+ $("#imgBtn-4").click(function(){
+    $(".player-btn").css({
+        "color" : "#6c757d",
+        "border-color" : "#6c757d"
+    });
+    $("#btn-4-s, #btn-4-y").css({
+        "color" : "yellow",
+        "border-color" : "yellow",
+        "transition-timing-funtion" : "ease-in",
+    });
+ });
+
+ $(".player-btn").click(function(){
+    $(".player-btn").css({
+        "color" : "#6c757d",
+        "border-color" : "#6c757d"
+    });
+ });
+
+
+ $(".player-btn").hover(function(){
+    $(this).css("color", "white");
+ }, function(){
+    $(this).css({
+        "color" : "#6c757d",
+        "border-color" : "#6c757d"
+     } );
+ });
